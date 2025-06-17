51f5c0897cc6ddbdf38750efccd342b39302a204
YalagandhulaAkhil <yalagandhula23akhil@gmail.com>
2025-06-17 17:41:28 -0400

    removed .

--- main/main.tf
+++ main/main.tf
@@ -1,16 +1,16 @@
 module "s3" {
-  source      = "../modules/s3"
+  source      = "./modules/s3"
   bucket_name = var.bucket_name
 }
 
 module "dynamodb" {
-  source     = "../modules/dynamodb"
+  source     = "./modules/dynamodb"
   table_name = var.dynamodb_table_name
   hash_key   = var.dynamodb_hash_key
 }
 
 module "lambda" {
-  source        = "../modules/lambda"
+  source        = "./modules/lambda"
   function_name = var.lambda_function_name
   handler       = var.lambda_handler
   runtime       = var.lambda_runtime
